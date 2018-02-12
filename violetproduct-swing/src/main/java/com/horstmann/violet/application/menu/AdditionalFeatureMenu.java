6cce10e7c8a607fec80f5f58a25db93dcbcfe095
deepakkumarsharma <deeky.sharma@gmail.com>
2018-02-11 22:04:02 -0500

    initial work

---
+++ violetproduct-swing/src/main/java/com/horstmann/violet/application/menu/AdditionalFeatureMenu.java
@@ -0,0 +1,107 @@
+package com.horstmann.violet.application.menu;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+import com.horstmann.violet.application.gui.MainFrame;
+import com.horstmann.violet.framework.injection.resources.ResourceBundleInjector;
+import com.horstmann.violet.framework.injection.resources.annotation.ResourceBundleBean;
+import com.horstmann.violet.product.diagram.abstracts.AbstractGraph;
+
+
+@ResourceBundleBean(resourceReference = MenuFactory.class)
+public class AdditionalFeatureMenu extends JMenu
+{
+
+   @ResourceBundleBean(key = "additional")
+   public AdditionalFeatureMenu(final MainFrame mainFrame)
+   {
+       ResourceBundleInjector.getInjector().inject(this);
+       this.mainFrame = mainFrame;
+       this.createMenu();
+   }
+   private void createMenu()
+   {     
+       featureOneEnable.addActionListener(new ActionListener()
+       {
+           public void actionPerformed(ActionEvent event)
+           {
+               if (isThereAnyWorkspaceDisplayed()) {
+            	   AbstractGraph.feature1enabled = true;
+               }
+           }
+       });
+       this.add(featureOneEnable);
+ 
+       featureOneDisable.addActionListener(new ActionListener()
+       {
+           public void actionPerformed(ActionEvent event)
+           {
+               if (isThereAnyWorkspaceDisplayed()) {
+            	   AbstractGraph.feature1enabled = false;
+               }
+           }
+       });
+       this.add(featureOneDisable);       
+
+       featureTwoEnable.addActionListener(new ActionListener()
+       {
+           public void actionPerformed(ActionEvent event)
+           {
+               if (isThereAnyWorkspaceDisplayed()) {
+            	   AbstractGraph.feature2enabled = true;
+               }
+           }
+       });
+       this.add(featureTwoEnable);
+       
+       featureTwoDisable.addActionListener(new ActionListener()
+       {
+           public void actionPerformed(ActionEvent event)
+           {
+               if (isThereAnyWorkspaceDisplayed()) {
+            	   AbstractGraph.feature2enabled = false;
+               }
+           }
+       });
+       this.add(featureTwoDisable);    
+
+       calculateCBO.addActionListener(new ActionListener()
+       {
+           public void actionPerformed(ActionEvent event)
+           {
+               if (isThereAnyWorkspaceDisplayed()) {
+            	  
+               }
+           }
+       });
+       this.add(calculateCBO);    
+       
+   }
+
+   private boolean isThereAnyWorkspaceDisplayed()
+   {
+       return mainFrame.getWorkspaceList().size() > 0;
+   }
+   
+   /** Application frame */
+   private MainFrame mainFrame;
+
+   @ResourceBundleBean(key = "additional.featureoneenable")
+   private JMenuItem featureOneEnable;
+
+   @ResourceBundleBean(key = "additional.featureonedisable")
+   private JMenuItem featureOneDisable;
+
+   @ResourceBundleBean(key = "additional.featuretwoenable")
+   private JMenuItem featureTwoEnable;
+
+   @ResourceBundleBean(key = "additional.featuretwodisable")
+   private JMenuItem featureTwoDisable;
+
+   @ResourceBundleBean(key = "additional.calculate_CBO")
+   private JMenuItem calculateCBO;
+}
